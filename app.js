b77bc2d2c1214f9312e445dc22d35059c3c9601e
KayzPart <particulier.flore@gmail.com>
2021-11-16 15:21:01 +0100

    feat: touchEvent

--- app.js
+++ app.js
@@ -45,6 +45,14 @@ window.onload = () => {
         canvas.strokeCercle();
     })
     
+    function startup() {
+        var el = document.getElementsByTagName("canvas")[0];
+        el.addEventListener("touchstart", handleStart, false);
+        el.addEventListener("touchend", handleEnd, false);
+        el.addEventListener("touchcancel", handleCancel, false);
+        el.addEventListener("touchleave", handleLeave, false);
+        el.addEventListener("touchmove", handleMove, false);
+      }
 }
 
 
